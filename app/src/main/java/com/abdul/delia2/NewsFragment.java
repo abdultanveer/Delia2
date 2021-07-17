aaffeecae901f61d0354c808c60455e10841e625
abdul <abdul.tanveer@gmail.com>
2021-07-17 13:52:02 +0530

    Initial commit

---
+++ app/src/main/java/com/abdul/delia2/NewsFragment.java
@@ -0,0 +1,21 @@
+package com.abdul.delia2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NewsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+         super.onCreateView(inflater, container, savedInstanceState);
+        View newsFrag = inflater.inflate(R.layout.fragment_news,container,false);
+         return newsFrag;
+    }
+}
